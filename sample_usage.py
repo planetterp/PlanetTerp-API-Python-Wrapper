4ad551ff172f7bc1de116345d53f640b22cb1116
Ethan Schaffer <ethan@schaffer.com>
2020-06-22 14:29:17 -0500

    Uncomment Sample Calls

--- sample_usage.py
+++ sample_usage.py
@@ -6,12 +6,12 @@ def pretty_json(json_object, indentation = 2):
 
 pt = PlanetTerp.PlanetTerp()
 
-#print(pretty_json(pt.course(name="CMSC132", reviews="true")))
+print(pretty_json(pt.course(name="CMSC132", reviews="true")))
 
-#print(pretty_json(pt.all_courses(department="CMSC", limit="3")))
+print(pretty_json(pt.all_courses(department="CMSC", limit="3")))
 
-#print(pretty_json(pt.professors("Fawzi Emad", reviews="true")))
+print(pretty_json(pt.professors("Fawzi Emad", reviews="true")))
 
-#print(pretty_json(pt.all_professors(type="ta", reviews="true", limit="2")))
+print(pretty_json(pt.all_professors(type="ta", reviews="true", limit="2")))
 
-#print(pretty_json(pt.grades(course="CMSC132", professor="Fawzi Emad")))
+print(pretty_json(pt.grades(course="CMSC132", professor="Fawzi Emad")))
