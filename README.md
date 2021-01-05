3ac837f0da39f5ad3a588279484468f5cb60f549
Liam DeVoe <orionldevoe@gmail.com>
2021-01-05 16:32:16 -0500

    apply review comments

--- README.md
+++ README.md
@@ -5,21 +5,22 @@ This is a thin python wrapper around the [PlanetTerp API](http://api.planetterp.
 ### Usage
 
 ```python
-from planetterp import course, all_courses, professor, all_professors, grades
+from planetterp import course, courses, professor, professors, grades
 import json
 
 def pretty_json(json_object, indentation = 2):
     return json.dumps(json_object, indent=indentation)
 
 course_ = course(name="CMSC132", reviews="true")
-courses = all_courses(department="CMSC", limit="3")
+courses_ = courses(department="CMSC", limit="3")
 prof = professor("Fawzi Emad", reviews="true")
-profs = all_professors(type_="ta", reviews="true", limit="2")
+profs = professors(type_="ta", reviews="true", limit="2")
 grades_ = grades(course="CMSC132", professor="Fawzi Emad")
 
 print(pretty_json(course_))
-print(pretty_json(courses))
+print(pretty_json(courses_))
 print(pretty_json(prof))
 print(pretty_json(profs))
 print(pretty_json(grades_))
+
 ```
--- planetterp/PlanetTerp.py
+++ planetterp/PlanetTerp.py
@@ -15,35 +15,37 @@ def course(name, reviews = False):
     reviews: bool, optional
         Whether to also return reviews for the course, specifically reviews for
         professors that taught the course and have the course listed as the one
-        being reviewed.
+        being reviewed. Defaults to False.
     """
     params = {"name" : name, "reviews": "true" if reviews else "false"}
     url = BASE_URL + "course?" + urlencode(params)
     return requests.get(url).json()
 
 
-def all_courses(department = None, reviews = False, limit = 100, offset = 0):
+def courses(department = None, reviews = False, limit = 100, offset = 0):
     """
     Get all courses meeting some criteria.
 
     Parameters
     ----------
-    department: string
+    department: string, optional
         Only return courses in the given department. The department name must be
         four characters. Defaults to all departments.
-    reviews: bool
+    reviews: bool, optional
         Also return reviews for the course, specifically reviews for
         professors that taught the course and have the course listed as the one
-        being reviewed.
-    limit: int
+        being reviewed. Defaults to False.
+    limit: int, optional
         Maximum number of courses to return. Must be between 1 and 1000
         inclusive. Defaults to 100.
-    offset: int
+    offset: int, optional
         Number of courses to skip (offered for pagination). Defaults to 0.
     """
     params = {"department" : department,
               "reviews": "true" if reviews else "false",
               "limit": limit, "offset": offset}
+    # fitler out None args
+    params = {k:v for k, v in params.items() if v is not None}
     url = BASE_URL + "courses?" + urlencode(params)
     return requests.get(url).json()
 
@@ -65,22 +67,22 @@ def professor(name, reviews = False):
     return requests.get(url).json()
 
 
-def all_professors(type_ = None, reviews = False, limit = None, offset = None):
+def professors(type_ = None, reviews = False, limit = 100, offset = 0):
     """
     Get all professors meeting some criteria.
 
     Parameters
     ----------
-    type_: {"professor", "ta"}
+    type_: {"professor", "ta"}, optional
         Only return reviews for the specified instructor type, either professors
         or TAs. Defaults to returning both.
-    reviews: bool
+    reviews: bool, optional
         Whether to also return reviews for the instructors. Defaults to false.
-    limit: int
-        Maximum number of courses to return. Must be between 1 and 1000
+    limit: int, optional
+        Maximum number of instructors to return. Must be between 1 and 1000
         inclusive. Defaults to 100.
-    offset: int
-        Number of courses to skip (offered for pagination). Defaults to 0.
+    offset: int, optional
+        Number of instructors to skip (offered for pagination). Defaults to 0.
     """
     if type_ not in ["professor", "ta"]:
         raise ValueError("Expected type to be on of ['professor', 'ta'], got "
@@ -101,13 +103,22 @@ def grades(course = None, professor = None, semester = None, section = None):
     Parameters
     ----------
     course: str
-        Limit the grades returned to only this course.
+        Limit the grades returned to only this course. Defaults to all courses.
+        Either `course` or `professor` must be passed.
     professor: str
-        The name of the professor to return grades for.
-    semester: str
+        The name of the professor to return grades for. Defaults to all
+        professors. Either `course` or `professor` must be passed.
+    semester: str, optional
         The semester to return grades for. This should be passed as the year
         followed by the semester code. The spring semester code is 01 and the
         fall semester code is 08. For example, 202001 is Spring 2020.
+    section: str, optional
+        Limit the grades returned to only this section. Defaults to all
+        sections.
+
+    Notes
+    -----
+    Either `course` or `professor` must be passed.
     """
     params = {"course" : course, "professor": professor, "semester": semester,
               "section": section}
